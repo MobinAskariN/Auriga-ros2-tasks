a2dfdfb35f180866a88514276619182023559859
radin <hajradin1999@gmail.com>
2025-10-30 11:39:59 +0330

    second edit no speed controll

--- src/cpp_package/src/main.cpp
+++ src/cpp_package/src/main.cpp
@@ -1,22 +1,24 @@
 #include <cmath>
 
 
+
 // we use stanley to controll steering anlge
-class Stanley_Pid{
+class Controller{
 
     const int lenght {1};
-    float cross_track_error{0}, heading_error{0}, kappa{0}, Steering_angle{0}, velocity {0}, curve_intensity {0};
-    float k_s {0.1}; // to avoid division by zero
+    float cross_track_error{0}, heading_error{0}, kappa{0}, Steering_angle{0}, velocity {0};
     //tuning constants
     float k {0};
     float k_p {0};
+    float k_ff {0};
     
 
     public:
     
         float calculate_steering_angle(){
 
-            Steering_angle = heading_error + atan(k * cross_track_error / (k_s + velocity)) + atan(lenght * kappa);
+            velocity = std::max(velocity, 0.001f); // to avoid division by zero
+            Steering_angle = heading_error + atan(k * cross_track_error / (velocity)) +  k_ff * atan(lenght * kappa);
 
         } 
 
